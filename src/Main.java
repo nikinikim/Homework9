ba57b0d113e91afcd6fcabee81a9c98948de0c65
Niki <colt92@inbox.ru>
2022-10-18 20:00:20 +0500

    Внесены правки, согласно замечаниям

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import javax.swing.plaf.IconUIResource;
-import java.util.Arrays;
 public class Main {
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
@@ -10,13 +8,13 @@ public class Main {
         return arr;
     }
     public static void main(String[] args) {
-        Task1();
-        Task2();
-        Task3();
-        Task4();
+        task1();
+        task2();
+        task3();
+        task4();
     }
 
-    public static void Task1() {
+    public static void task1() {
         int[] arr = generateRandomArray();
         int sumOfSpending = -1;
         for (int j : arr) {
@@ -24,9 +22,9 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила " + sumOfSpending + " рублей");
     }
-    public static void Task2() {
+    public static void task2() {
         int[] arr = generateRandomArray();
-        int maxSpending = -1;
+        int maxSpending = 0;
         int minSpending = 200000;
         for (int current : arr) {
             if (current > maxSpending) {
@@ -39,20 +37,20 @@ public class Main {
         System.out.println("Минимальная сумма трат за день составила " + minSpending + " рублей. Максимальная сумма трат за день составила " + maxSpending + " рублей.");
     }
 
-    public static void Task3(){
+    public static void task3(){
         int[] arr = generateRandomArray();
-        int sumOfSpending = -1;
-        int averageSpending = sumOfSpending;
+        int sumOfSpending = 0;
+        double averageSpending = 0;
         for (int current : arr) {
             if (current > sumOfSpending) {
                 sumOfSpending += current;
-                averageSpending = sumOfSpending / arr.length;
             }
         }
+        averageSpending = (double)sumOfSpending / arr.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageSpending + " рублей.");
     }
 
-    public static void Task4(){
+    public static void task4(){
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
